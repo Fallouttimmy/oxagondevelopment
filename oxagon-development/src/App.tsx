c9399829e3f29b3fb951daa325fd815500698a4f
Fallouttimmy <Timmethy476@gmail.com>
2026-07-01 02:36:31 +0200

    Fix page title updates on route change and reload

--- oxagon-development/src/App.tsx
+++ oxagon-development/src/App.tsx
@@ -1,26 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Policies from "./pages/Policies";
 
 function AppContent() {
-  const handleTitleUpdate = (title: string) => {
-    document.title = title;
-  };
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      document.title = "Homepage - Oxagon Development";
+    } else if (location.pathname === "/policies") {
+      document.title = "Policies - Oxagon Development";
+    } else {
+      document.title = "Oxagon Development";
+    }
+  }, [location.pathname]);
 
   return (
     <>
       <Navbar />
       <Routes>
-        <Route 
-          path="/" 
-          element={<Home onTitleChange={() => handleTitleUpdate("Homepage - Oxagon Development")} />} 
-        />
-        <Route 
-          path="/policies" 
-          element={<Policies onTitleChange={() => handleTitleUpdate("Policies - Oxagon Development")} />} 
-        />
+        <Route path="/" element={<Home />} />
+        <Route path="/policies" element={<Policies />} />
       </Routes>
     </>
   );
