679016415c70e07de4daeabbbb0c04c572ed7364
Fallouttimmy <Timmethy476@gmail.com>
2026-07-01 02:31:34 +0200

    Add dynamic page title updates based on route

--- oxagon-development/src/App.tsx
+++ oxagon-development/src/App.tsx
@@ -1,16 +1,35 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Policies from "./pages/Policies";
 
+function AppContent() {
+  const handleTitleUpdate = (title: string) => {
+    document.title = title;
+  };
+
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route 
+          path="/" 
+          element={<Home onTitleChange={() => handleTitleUpdate("Homepage - Oxagon Development")} />} 
+        />
+        <Route 
+          path="/policies" 
+          element={<Policies onTitleChange={() => handleTitleUpdate("Policies - Oxagon Development")} />} 
+        />
+      </Routes>
+    </>
+  );
+}
+
 export default function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/policies" element={<Policies />} />
-      </Routes>
+      <AppContent />
     </Router>
   );
 }
