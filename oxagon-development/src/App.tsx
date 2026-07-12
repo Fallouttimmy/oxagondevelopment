ad769a8cb61a5c94ea0d5dc9cd8dada930480697
Fallouttimmy <Timmethy476@gmail.com>
2026-07-12 23:41:46 +0200

    App: add Explore, OSN, Advertise, Creator routes and page titles

--- oxagon-development/src/App.tsx
+++ oxagon-development/src/App.tsx
@@ -3,6 +3,10 @@ import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Policies from "./pages/Policies";
+import Explore from "./pages/Explore";
+import OSN from "./pages/OSN";
+import Advertise from "./pages/Advertise";
+import Creator from "./pages/Creator";
 
 function AppContent() {
   const location = useLocation();
@@ -12,6 +16,14 @@ function AppContent() {
       document.title = "Homepage - Oxagon Development";
     } else if (location.pathname === "/policies") {
       document.title = "Policies - Oxagon Development";
+    } else if (location.pathname === "/explore") {
+      document.title = "Explore - Oxagon Development";
+    } else if (location.pathname === "/osn") {
+      document.title = "OSN - Oxagon Development";
+    } else if (location.pathname === "/advertise") {
+      document.title = "Advertisement Program - Oxagon Development";
+    } else if (location.pathname === "/creator") {
+      document.title = "Creator Program - Oxagon Development";
     } else {
       document.title = "Oxagon Development";
     }
@@ -23,6 +35,10 @@ function AppContent() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/policies" element={<Policies />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/osn" element={<OSN />} />
+        <Route path="/advertise" element={<Advertise />} />
+        <Route path="/creator" element={<Creator />} />
       </Routes>
     </>
   );
